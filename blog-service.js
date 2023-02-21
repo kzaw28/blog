91d69073380931829492917b926edb80928034e6
Kaung Khant Zaw <kzaw@myseneca.ca>
2023-02-20 22:20:44 -0500

    - Add Post function

--- blog-service.js
+++ blog-service.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Kaung Khant Zaw Student ID: 157467218 Date: 20th February, 2023
+*
+*  Cyclic Web App URL: https://mushy-school-uniform-toad.cyclic.app/about
+*
+*  GitHub Repository URL: https://github.com/kzaw28/web322-app
+*
+********************************************************************************/ 
+
 const fs = require("fs");
 const { resolve } = require("path");
 
@@ -82,3 +95,94 @@ exports.getCategories = function() {
         resolve(categories);
     });
 };
+
+exports.addPost = function(postData) {
+    return new Promise(function(resolve, reject){
+        const isPublished = postData.published === "on";
+
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1; // Month is zero indexed
+        const day = today.getDate();
+
+
+        // Create a new blogPost object and give it values
+        let blogPost = {
+            id: posts.length + 1,
+            body: postData.body,
+            title: postData.title,
+            postDate: `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`,
+            category: postData.category,
+            featureImage: postData.featureImage,
+            published: isPublished
+        }
+
+        if (posts.push(blogPost))
+        {
+            resolve(blogPost);
+        } else {
+            reject("Post was not added");
+        }
+    });
+}
+
+exports.getPostsByCategory = function(category) {
+    return new Promise(function(resolve, reject){
+        let newArr = []
+        for (let key in posts) {
+            const post = posts[key]; // Need to deference the key
+            if (post.category == category)
+            {
+                newArr.push(post);
+            }
+        }
+    
+        if (newArr.length == 0)
+        {
+            reject("No result returned");
+        } else {
+            resolve(newArr);
+        }
+    });
+}
+
+exports.getPostsByMinDate = function(minDateStr) {
+    return new Promise(function(resolve, reject){
+        let newArr = [];
+        for (let key in posts) {
+            const post = posts[key];
+            if (new Date(post.postDate) >= new Date(minDateStr))
+            {
+                newArr.push(post);
+            }
+        }
+    
+        if (newArr.length == 0)
+        {
+            reject("No result returned");
+        } else {
+            resolve(newArr);
+        }
+    });
+}
+
+
+exports.getPostsById = function(id) {
+    return new Promise(function(resolve, reject){
+        let tempPost = {};
+        for (let key in posts) {
+            const post = posts[key];
+            if (post.id == id)
+            {
+                tempPost = post;
+            }
+        }
+    
+        if (Object.keys(tempPost).length === 0)
+        {
+            reject("No result returned");
+        } else {
+            resolve(tempPost);
+        }
+    });
+}
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Kaung Khant Zaw Student ID: 157467218 Date: 1st February, 2023
+*  Name: Kaung Khant Zaw Student ID: 157467218 Date: 20th February, 2023
 *
 *  Cyclic Web App URL: https://mushy-school-uniform-toad.cyclic.app/about
 *
@@ -13,19 +13,37 @@
 
 
 
-var path = require("path");
-var express = require("express");
-var app = express();
+const path = require("path");
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const cloudinary= require("cloudinary").v2;
+const streamifier = require("streamifier");
 
-var data = require("./blog-service")
+const data = require("./blog-service")
 
+
+// Cloudinary config ---------------------------------
+cloudinary.config(
+    {
+        cloud_name: "dojcvsabu",
+        api_key: "432299183778232",
+        api_secret: "P6BxLJXX9Cu-Tw96UqFIVYFq7w8",
+        secure: true
+    }
+);
+
+// Multer variable without disk storage  --------------
+const upload = multer();
+
+// Port -----------------------------------------------
 var HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
     console.log("Express http server listening on " + HTTP_PORT);
 };
 
-// Serving static files
+// Serving static files ------------------------------
 app.use(express.static(path.join(__dirname, "public"))); 
 
 // ROUTES ----------------------------------
@@ -41,46 +59,150 @@ app.get("/about", function(req, res){
 
 app.get("/blog", function(req, res){
     data.getPublishedPosts()
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            var returnObj = {
-                message: `${err}`
-            };
-            res.send(returnObj)
-        }) 
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        let returnObj = {
+            message: `${err}`
+        };
+        res.send(returnObj);
+    }) 
 });
 
+
+app.get("/posts/add", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/addPost.html"));
+})
+
+
 app.get("/posts", function(req, res){
-    data.getAllPosts()
+    const category = req.query.category;
+    const minDateStr = req.query.minDate;
+
+    if (category)
+    {
+        data.getPostsByCategory(category)
         .then((data) => {
             res.send(data);
         })
         .catch((err) => {
-            var returnObj = {
+            let returnObj = {
                 message: `${err}`
             };
-            res.send(returnObj)
+            res.send(returnObj);
+        });
+    } else if (minDateStr) {
+        data.getPostsByMinDate(minDateStr)
+        .then((data) => {
+            res.send(data);
         })
+        .catch((err) => {
+            let returnObj = {
+                message: `${err}`
+            };
+            res.send(returnObj);
+        })
+    } else 
+    {
+        data.getAllPosts()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            let returnObj = {
+                message: `${err}`
+            };
+            res.send(returnObj);
+        })
+    }
 });
+
+
+app.get("/posts/:id", function(req, res){
+    const postID = req.params.id;
+    data.getPostsById(postID)
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        let returnObj = {
+            message: `${err}`
+        };
+        res.send(returnObj);
+    })
+})
 
 app.get("/categories", function(req, res){
     data.getCategories()
-        .then((data) => {
-            res.send(data);
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        let returnObj = {
+            message: `${err}`
+        };
+        res.send(returnObj)
+    })
+});
+
+// Single fileupload with the name "image" in the req, adds a 'file' object to the req
+app.post("/posts/add", upload.single('featureImage'), function(req, res){
+
+    // If the file was uploaded to the 'req'
+    if (req.file)
+    {
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                // Upload file to cloudinary
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded) => {
+            processPost(uploaded.url);
+        });
+    } else {
+        processPost("");
+    }
+
+    // Process Post Function Definition ----------------------------
+    function processPost(imageUrl) {
+        req.body.featureImage = imageUrl;
+        data.addPost(req.body)
+        .then((newPost)=> {
+            res.redirect("/posts");
         })
         .catch((err) => {
-            var returnObj = {
+            let returnObj = {
                 message: `${err}`
             };
             res.send(returnObj)
         })
+    }    
 });
 
 app.get('*', function(req, res){
     res.status(404).send("Page Not Found");
 });
+// ----------------------------------------------
+
+
 
 data
     .initialize()
